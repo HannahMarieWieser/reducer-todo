1d696a73d09d083e1e63c50e8aba16b70900e5a9
HannahMarieWieser <nikauli@gmail.com>
2019-08-13 18:22:08 -0400

    refactoring from class components

--- reducer-todo/src/components/todolist.js
+++ reducer-todo/src/components/todolist.js
@@ -2,48 +2,21 @@ import React, { useState, useReducer } from 'react';
 
 import { initialState, titleReducer } from '../reducers/reducer';
 
-const Title = () => {
-    const [newTitleText, setNewTitleText] = useState();
+class TodoList extends React.Component {
+    constructor(props) {
+      super(props);
+      console.log('TodoList', props);
+    }
+    render() {
+      console.log('TodoList', this.props);
+      return (
+        <div>
+          {this.props.todos.map(todo => <Todo key={todo.id} todo={todo}
+            toggleCompleted={this.props.toggleCompleted}/>)}
+        </div>
+      );
+    }
+  }
   
-    // useReducer - takes in a reducer, and an initialState obj
-    // returns - a state obj, and the dispatch fn
-    const [state, dispatch] = useReducer(titleReducer, initialState);
-    console.log(state);
+  export default TodoList;
   
-    const handleChanges = e => {
-      setNewTitleText(e.target.value);
-    };
-  
-    return (
-      <div>
-        {!state.editing ? (
-          <h1>
-            {state.title}{' '}
-            <i
-              className="far fa-edit"
-              onClick={() => dispatch({ type: 'TOGGLE_EDITING' })}
-            />
-          </h1>
-        ) : (
-          <div>
-            <input
-              className="title-input"
-              type="text"
-              name="newTitleText"
-              value={newTitleText}
-              onChange={handleChanges}
-            />
-            <button
-              onClick={() =>
-                dispatch({ type: 'UPDATE_TITLE', payload: newTitleText })
-              }
-            >
-              Update title
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  };
-  
-  export default Title;
